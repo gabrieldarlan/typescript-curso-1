7f8ae61c957a1c5e41b595c043efe34c9c7fb6f9
gdarlan <gabriel.darlan88@gmail.com>
2022-03-26 14:09:06 -0300

    curso 2 capitulo 4 completo

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -20,7 +20,11 @@ export class NegociacaoController {
   }
 
   public adiciona(): void {
-    const negociacao = this.criaNegociacao();
+    const negociacao = Negociacao.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
     if (this.ehDiaUtil(negociacao)) {
       this.negociacoes.adiciona(negociacao);
       this.atualizaView();
@@ -35,14 +39,6 @@ export class NegociacaoController {
       negociacao.data.getDay() > DiasDaSemana.DOMINGO &&
       negociacao.data.getDay() < DiasDaSemana.SABADO
     );
-  }
-
-  private criaNegociacao(): Negociacao {
-    const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ','));
-    const quantidade = parseInt(this.inputQuantidade.value);
-    const valor = parseFloat(this.inputValor.value);
-    return new Negociacao(date, quantidade, valor);
   }
 
   private limparFormulario(): void {
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -14,7 +14,7 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
     adiciona() {
-        const negociacao = this.criaNegociacao();
+        const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         if (this.ehDiaUtil(negociacao)) {
             this.negociacoes.adiciona(negociacao);
             this.atualizaView();
@@ -27,13 +27,6 @@ export class NegociacaoController {
     ehDiaUtil(negociacao) {
         return (negociacao.data.getDay() > DiasDaSemana.DOMINGO &&
             negociacao.data.getDay() < DiasDaSemana.SABADO);
-    }
-    criaNegociacao() {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
     limparFormulario() {
         this.inputData.value = '';
